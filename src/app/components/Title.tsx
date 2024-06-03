b98b355fe48d9390fdf19be60c4c69715148bed1
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-06-03 19:08:38 +0900

    サービスページの作成

---
+++ src/app/components/Title.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface TitleProps {
+  title: string;
+}
+
+const Title: React.FC<TitleProps> = ({title}) => {
+  return (
+    <h2>
+      <h2 className='text-2xl font-semibold'>
+        {title}
+      </h2>
+    </h2>
+  )
+}
+
+export default Title
